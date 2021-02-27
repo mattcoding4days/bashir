9f3e548505acbc0aeef98a2e9980848a441a08ff
Matt Williams <mattinzhuhai@outlook.com>
2021-02-27 14:13:02 -0800

    :hammer:  Added example version test for pytest conf

---
+++ test/unittest/test_generics.py
@@ -0,0 +1,13 @@
+"""
+An example for how to use the conftest.py fixture
+design patter, with unittests
+"""
+
+import pytest
+
+
+def test_version(version_test) -> None:
+    """
+    Test the package version number
+    """
+    assert version_test.__version__ == '0.1.0'
